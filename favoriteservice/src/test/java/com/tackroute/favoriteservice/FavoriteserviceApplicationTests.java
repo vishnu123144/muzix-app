e5e1180eb1ba112c7232889b3a6402adee4ed2c1
mitaligarg10 <mitaligarg94@gmail.com>
2021-06-10 16:34:30 +0530

    initial commit

---
+++ favoriteservice/src/test/java/com/tackroute/favoriteservice/FavoriteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tackroute.favoriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-cloud-gateway/src/main/java/com/stackroute/usermanagementservice/GatewayApplicationMain.java
@@ -0,0 +1,15 @@
+package com.stackroute.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplicationMain.class, args);
+    }
+
+}
