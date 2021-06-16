9b472dc2beab0cd23f6bd633b160b21f29ce19ea
MuramVishnu.Reddy <muramvishnu.reddy@brillio.com>
2021-06-16 15:23:30 +0530

    initial commit

--- searchservice/src/main/java/com/stackroute/searchservice/config/SearchConfig.java
+++ searchservice/src/main/java/com/stackroute/searchservice/config/SearchConfig.java
@@ -1,8 +1,7 @@
 package com.stackroute.searchservice.config;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 @Configuration
@@ -10,10 +9,7 @@ public class SearchConfig {
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
-
         return new RestTemplate();
-
     }
 
-}
-
+    }
